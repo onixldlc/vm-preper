775a2346e55a842374f2d26274654b6968009930
onixldlc <81515667+onixldlc@users.noreply.github.com>
2024-02-28 16:31:08 +0700

    Create dockerized-openvpn.sh

---
+++ dockerized-openvpn.sh
@@ -0,0 +1,12 @@
+cd ~
+mkdir vpn
+git clone https://github.com/kylemanna/docker-openvpn.git
+cd docker-openvpn/
+sudo docker build -t myownvpn .
+cd ..
+mkdir vpn-data && touch vpn-data/vars
+sudo docker run -v $PWD/vpn-data:/etc/openvpn --rm myownvpn ovpn_genconfig -u udp://$1:3000
+sudo docker run -v $PWD/vpn-data:/etc/openvpn --rm -it myownvpn ovpn_initpki
+sudo docker run -v $PWD/vpn-data:/etc/openvpn -d -p 3000:1194/udp --cap-add=NET_ADMIN myownvpn
+docker run -v $PWD/vpn-data:/etc/openvpn --rm -it myownvpn easyrsa build-client-full $2 nopass
+docker run -v $PWD/vpn-data:/etc/openvpn --rm myownvpn ovpn_getclient $2 > $2.ovpn
