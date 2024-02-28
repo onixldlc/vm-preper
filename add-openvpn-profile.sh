3dc62a0f217361a6cabf781b342bfaa3587a4251
onixldlc <81515667+onixldlc@users.noreply.github.com>
2024-02-28 16:38:15 +0700

    Update add-openvpn-profile.sh

--- add-openvpn-profile.sh
+++ add-openvpn-profile.sh
@@ -1,2 +1,3 @@
+cd ~/vpn
 sudo docker run -v $PWD/vpn-data:/etc/openvpn --rm -it myownvpn easyrsa build-client-full $2 nopass
 sudo docker run -v $PWD/vpn-data:/etc/openvpn --rm myownvpn ovpn_getclient $2 > $2.ovpn
