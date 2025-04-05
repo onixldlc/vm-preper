d73d902431ed0666ca1bc0cb9be3242ef68a6adf
onixldlc <81515667+onixldlc@users.noreply.github.com>
2025-04-05 22:47:44 +0700

    add: create init script to start everything

---
+++ init.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# check if the script is running on ubuntu environment
+if [ -f /etc/lsb-release ]; then
+    echo "Running on Ubuntu environment."
+    echo "starting the installer..."
+    curl -o- "https://raw.githubusercontent.com/onixldlc/vm-preper/refs/heads/main/script/setup/ubuntu/install-tools.sh" | sh 
+else
+    echo "Currently this script is only intended to run on Ubuntu environment."
+    exit 1
+fi
