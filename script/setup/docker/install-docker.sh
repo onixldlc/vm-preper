7050a07553f591c9d1886bb6fce657974a4b5d54
onixldlc <81515667+onixldlc@users.noreply.github.com>
2025-06-29 00:41:00 +0700

    fix: fixed missing sudo in docker installation

--- script/setup/docker/install-docker.sh
+++ script/setup/docker/install-docker.sh
@@ -25,4 +25,4 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
-apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
